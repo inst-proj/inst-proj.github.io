d005f5aaa99d59d7455c056aa0e1846d4ec0f34d
Artem Borovoy <speedy228228@gmail.com>
2018-04-07 17:59:53 +0300

    second

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.yak').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000,); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
+});
+
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
+
+(function () {
+    $(document).on('click', '.search-button', function () {
+        $(this).parent().parent().toggleClass('active');
+    });
+})();
